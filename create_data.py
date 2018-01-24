9c5e1f3c10510801674d788bdd558e38135030f2
Boris Omelin <elros.ob@gmail.com>
2018-01-24 03:28:59 +0300

    Added keyword args, checking for __main__

--- create_data.py
+++ create_data.py
@@ -1,18 +1,16 @@
-import sys, csv, random
+import sys, csv, random, argparse
 
 
 class DataWriter:
-    def __init__(self, employees=300, skills=30, rating=5):
-        try:
-            self.employees = int(employees)
-            self.skills = int(skills)
-            self.rating = int(rating)
-        except Exception:
-            sys.exit('Please enter correct values for employees, skills and rating.')
+    def __init__(self, filename, employees=300, skills=30, rating=5):
+        self.filename = filename
+        self.employees = employees
+        self.skills = skills
+        self.rating = rating
 
     def create_csv(self):
         """Create csv with pseudo-ratings for employees"""
-        with open('data.csv', 'w', newline='') as file:
+        with open(self.filename, 'w', newline='') as file:
             writer = csv.writer(file)
             for employee in range(self.employees):
                 record = [employee+1]
@@ -20,5 +18,18 @@ class DataWriter:
                 writer.writerow(record)
 
 
-data = DataWriter(*sys.argv[1:])
-data.create_csv()
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, help="name of file to write data")
+    parser.add_argument("-e", "--employees", type=int, help="count of employees with ratings in datafile")
+    parser.add_argument("-s", "--skills", type=int, help="count of skills for each employee")
+    parser.add_argument("-r", "--rating", type=int, help="maximum of rating value")
+    args = parser.parse_args()
+    defined = {k: v for k, v in vars(args).items() if v is not None}
+
+    return defined
+
+
+if __name__ == "__main__":
+    data = DataWriter(**cli())
+    data.create_csv()
