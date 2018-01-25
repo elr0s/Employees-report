6a4e9ddbd56d34adfcab80d2436e9211a1b2e703
Boris Omelin <elros.ob@gmail.com>
2018-01-25 03:18:04 +0300

    add file exist validation; add keywords args at report.py; pep8 spec

--- create_data.py
+++ create_data.py
@@ -1,4 +1,9 @@
-import sys, csv, random, argparse
+#!/usr/bin/python3
+import os
+import sys
+import csv
+import random
+import argparse
 
 
 class DataWriter:
@@ -8,17 +13,28 @@ class DataWriter:
         self.skills = skills
         self.rating = rating
 
+    def _check_file(self):
+        if os.path.exists(self.filename):
+            answer = input('File with that name already exists.\nDo you want to rewrite it? (y/n) ')
+            if answer == 'n':
+                sys.exit()
+            elif answer == 'y':
+                return
+            self._check_file()
+
     def create_csv(self):
         """Create csv with pseudo-ratings for employees"""
+        self._check_file()
         with open(self.filename, 'w', newline='') as file:
             writer = csv.writer(file)
             for employee in range(self.employees):
-                record = [employee+1]
-                record += [random.choice(range(1, self.rating+1)) for skill in range(self.skills)]
+                record = [employee + 1]
+                record += [random.choice(range(1, self.rating + 1)) for skill in range(self.skills)]
                 writer.writerow(record)
+        print('File with test data "{0}" was created.'.format(self.filename))
 
 
-def cli():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=str, help="name of file to write data")
     parser.add_argument("-e", "--employees", type=int, help="count of employees with ratings in datafile")
@@ -27,9 +43,5 @@ def cli():
     args = parser.parse_args()
     defined = {k: v for k, v in vars(args).items() if v is not None}
 
-    return defined
-
-
-if __name__ == "__main__":
-    data = DataWriter(**cli())
+    data = DataWriter(**defined)
     data.create_csv()
--- report.py
+++ report.py
@@ -1,20 +1,23 @@
-import sys, csv, math
+#!/usr/bin/python3
+import os
+import csv
+import sys
+import argparse
 from collections import OrderedDict
+
 
 class Report:
     def __init__(self, filename, group_size=3):
-        try:
             self.filename = filename
-            self.group_size = int(group_size)
-        except Exception:
-            sys.exit('Please enter correct values for group size.')
+            self.group_size = group_size
 
     def _calculate_medians(self, data):
         medians = []
         for ratings in data.values():
             i = 0
             while len(ratings) > 0:
-                if len(medians) < i+1: medians.append(0)
+                if len(medians) < i + 1:
+                    medians.append(0)
                 medians[i] += sum(ratings[:self.group_size])
                 ratings = ratings[self.group_size:]
                 i += 1
@@ -35,10 +38,18 @@ class Report:
             data[code] = groups
         return data
 
+    def _check_data(self):
+        if not os.path.exists(self.filename):
+            print('Data file with that name doesn\'t exists.')
+            sys.exit()
+
     def write_report(self):
-        """Make report with average values and 
-           difference between own and general for
-           each skills group of each employee"""
+        """
+        Make report with average values and
+        difference between own and general for
+        each skills group of each employee.
+        """
+        self._check_data()
         with open(self.filename, newline='') as datafile, open('report.csv', 'w', newline='') as reportfile:
             reader = csv.reader(datafile)
 
@@ -53,12 +64,19 @@ class Report:
 
             titles = ['Code']
             for rating in range(len(medians)):
-                titles.append('Skills group {0}'.format(rating+1))
+                titles.append('Skills group {0}'.format(rating + 1))
             writer.writerow(titles)
 
             for code, ratings in report.items():
                 writer.writerow([code] + ratings)
 
 
-report = Report(*sys.argv[1:])
-report.write_report()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, help="name of file with employees data")
+    parser.add_argument("-g", "--group_size", type=int, help="count of skills in skills group")
+    args = parser.parse_args()
+    defined = {k: v for k, v in vars(args).items() if v is not None}
+
+    report = Report(**defined)
+    report.write_report()
